fe7c9ef6a5d85b43f6f7ed42624d258dd317dee7
LutzGebelman <yuraosipov2012@gmail.com>
2025-02-20 20:31:02 +0200

    Reformat

--- src/recipeBookApplication/Recipes.java
+++ src/recipeBookApplication/Recipes.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 
 public class Recipes {
 
-	private String name;
-	private List<String> ingredients;
+    private String name;
+    private List<String> ingredients;
     private String instructions;
     private String cuisine;
     private String mealType;
     private String difficultyLevel;
     private int cookingTime;
-    
-    public Recipes (String name, List<String>ingredients, String instructions,String cuisine, String mealType,String difficultyLevel, int cookingTime) {
+    public Recipes(String name, List<String> ingredients, String instructions, String cuisine, String mealType,
+            String difficultyLevel, int cookingTime) {
         this.name = name;
         this.ingredients = ingredients;
         this.instructions = instructions;
@@ -23,68 +23,73 @@ public class Recipes {
         this.difficultyLevel = difficultyLevel;
         this.cookingTime = cookingTime;
     }
-    
+
     public int getCookingTime() {
-		return cookingTime;
-	}
+        return cookingTime;
+    }
 
-	public String getDifficultyLevel() {
-		return difficultyLevel;
-	}
+    public String getDifficultyLevel() {
+        return difficultyLevel;
+    }
 
-	public void setDifficultyLevel(String difficultyLevel) {
-		this.difficultyLevel = difficultyLevel;
-	}
+    public void setDifficultyLevel(String difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
 
-	public void setCookingTime(int cookingTime) {
-		this.cookingTime = cookingTime;
-	}
+    public void setCookingTime(int cookingTime) {
+        this.cookingTime = cookingTime;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getCuisine() {
-		return cuisine;
-	}
+    public String getCuisine() {
+        return cuisine;
+    }
 
-	public void setCuisine(String cuisine) {
-		this.cuisine = cuisine;
-	}
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
 
-	public String getMealType() {
-		return mealType;
-	}
+    public String getMealType() {
+        return mealType;
+    }
 
-	public void setMealType(String mealType) {
-		this.mealType = mealType;
-	}
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<String> getIngredients() {
-		return ingredients;
-	}
+    public List<String> getIngredients() {
+        return ingredients;
+    }
 
-	public void setIngredients(List<String> ingredients) {
-		this.ingredients = ingredients;
-	}
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
 
-	public String getInstructions() {
-		return instructions;
-	}
+    public String getInstructions() {
+        return instructions;
+    }
 
-	public void setInstructions(String instructions) {
-		this.instructions = instructions;
-	}
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
 
-	public void displayRecipe() {
-        System.out.println("Recipe Name: " + name);
-        System.out.println("Cuisine: " + cuisine + " | Meal Type: " + mealType + " | Difficulty: " + difficultyLevel);
-        System.out.println("Cooking Time: " + cookingTime + " minutes");
-        System.out.println("Ingredients: " + ingredients);
-        System.out.println("Instructions: " + instructions);
+    public void displayRecipe() {
+        System.out.print("Recipe Name: " + name);
+        System.out.print("\n\r");
+        System.out.print("Cuisine: " + cuisine + " | Meal Type: " + mealType + " | Difficulty: " + difficultyLevel);
+        System.out.print("\n\r");
+        System.out.print("Cooking Time: " + cookingTime + " minutes");
+        System.out.print("\n\r");
+        System.out.print("Ingredients: " + ingredients);
+        System.out.print("\n\r");
+        System.out.print("Instructions: " + instructions);
+        System.out.print("\n\r");
     }
 }
