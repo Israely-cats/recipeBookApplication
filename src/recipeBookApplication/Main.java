35ebfec1092a888c668502b0063c93b07b4f802d
LutzGebelman <yuraosipov2012@gmail.com>
2025-02-20 20:26:41 +0200

    Add serialization interaction functions

--- src/recipeBookApplication/Main.java
+++ src/recipeBookApplication/Main.java
@@ -1,15 +1,52 @@
 package recipeBookApplication;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import recipeBookApplication.*;
 
 class RoachUtil {
     static int wrap(int d, int min, int max) {
         final int t = d < min ? max : d;
         return t > max ? min : t;
+    }
+
+    static void dumpData(String filename) {
+        try {
+            // Saving of object in a file
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+            // Method for serialization of object
+            out.writeObject(RecipesManagement.recipes);
+            out.close();
+            file.close();
+            System.out.println("Object has been serialized");
+        } catch (IOException ex) {
+            System.out.println("IOException is caught");
+            ex.printStackTrace();
+            System.exit(101);
+        }
+    }
+
+    static void loadData(String filename) {
+        try {
+            // Reading the object from a file
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(file);
+            RecipesManagement.recipes = (List<RecipesManagement>) in.readObject();
+            in.close();
+            file.close();
+            System.out.println("Object has been deserialized ");
+        } catch (IOException ex) {
+            System.out.println("IOException was caught while reading a file");
+            ex.printStackTrace();
+            System.exit(102);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            System.exit(103);
+        }
     }
 }
 
